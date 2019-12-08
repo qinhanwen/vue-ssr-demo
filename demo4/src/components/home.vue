f6b2318cf8bb906ff277c14545da823a02c3eb05
qinhanwen <285074175@qq.com>
2019-12-08 16:38:12 +0800

    feat:示例4 head注入

---
+++ demo4/src/components/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>home222</h1>
+</template>
+<script>
+  import titleMixin from '../mixins/title-mixin.js';
+  export default {
+    name: "home",
+    mixins: [titleMixin],
+    title() {
+      return 'Home页面';
+    },
+    data(){
+      return{
+         
+      }
+    }
+  }
+</script>
+<style scoped>
+
+</style>
